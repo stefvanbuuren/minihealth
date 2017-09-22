41483e336c663db3d813ef700bd59976411e2e29
Stef van Buuren <stef.vanbuuren@tno.nl>
2017-09-22 17:46:05 +0200

    Remove deprecated dplyr functions

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,6 @@
 #' @import methods donordata
 #' @importFrom stats predict
-#' @importFrom dplyr full_join arrange_ distinct_ %>% one_of
+#' @importFrom dplyr full_join arrange distinct %>%
 #' @importFrom clopus y2z z2y create.reference.call is.reference
 #' @importFrom brokenstick get_knots
 #' @importFrom rlang .data
