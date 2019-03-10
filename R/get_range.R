11db8b329fa730c1cbf8d74643ba7e45b9e7d550
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-03-10 20:18:35 +0100

    Add get.range()

---
+++ R/get_range.R
@@ -0,0 +1,36 @@
+#' Get a range from the antropometric data
+#'
+#' @param x An object of class \code{individual}.
+#' @param ynames A vector of name of outcome variable
+#' @param slotname The name of the slot, either \code{"x"} (often age),
+#' \code{"y"} (measure) or \code{"z"} (SDS).
+#' @return  A numeric vector of length 2 (if success), or
+#' \code{numeric(0)} (if failed)
+#' @examples
+#' # Find age range of anthropometric measures
+#' ind <- new("individual",
+#'            hgt = new("xyz", x = c(0.3, 2, 6)),
+#'            wgt = new("xyz", x = c(0.2, 1), y = c(1, NA)))
+#' get.range(ind)
+#' get.range(ind, slotname = "y")
+#' get.range(ind, ynames = c("hdc", "bmi"))
+#' @export
+get.range <- function(x,
+                      ynames = c("hgt", "wgt", "hdc", "bmi"),
+                      slotname = "x") {
+
+  if (!is.individual(x)) stop("Object not of S4 class 'individual'.")
+
+  ynames <- ynames[ynames %in% slotNames(x)]
+
+  # find the highest age across measurements
+  rng <- c(NA, NA)
+  for (yname in ynames) {
+    v <- slot(slot(x, yname), slotname)
+    if (sum(!is.na(v)) > 0L) rng <- range(c(v, rng), na.rm = TRUE, finite = TRUE)
+  }
+
+  if (any(is.na(rng))) rng <- numeric(0)
+  rng
+}
+
