33eb702b3ad2f60c40e6c8d113769ad6b0194cc6
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-03-14 12:17:06 +0100

    Rename to get_range()

--- R/get_range.R
+++ R/get_range.R
@@ -1,6 +1,6 @@
 #' Get a range from the antropometric data
 #'
-#' @param x An object of class \code{individual}.
+#' @param ind An object of class \code{individual}.
 #' @param ynames A vector of name of outcome variable
 #' @param slotname The name of the slot, either \code{"x"} (often age),
 #' \code{"y"} (measure) or \code{"z"} (SDS).
@@ -11,22 +11,22 @@
 #' ind <- new("individual",
 #'            hgt = new("xyz", x = c(0.3, 2, 6)),
 #'            wgt = new("xyz", x = c(0.2, 1), y = c(1, NA)))
-#' get.range(ind)
-#' get.range(ind, slotname = "y")
-#' get.range(ind, ynames = c("hdc", "bmi"))
+#' get_range(ind)
+#' get_range(ind, slotname = "y")
+#' get_range(ind, ynames = c("hdc", "bmi"))
 #' @export
-get.range <- function(x,
+get_range <- function(ind,
                       ynames = c("hgt", "wgt", "hdc", "bmi"),
                       slotname = "x") {
 
-  if (!is.individual(x)) stop("Object not of S4 class 'individual'.")
+  if (!is.individual(ind)) stop("Object not of S4 class 'individual'.")
 
-  ynames <- ynames[ynames %in% slotNames(x)]
+  ynames <- ynames[ynames %in% slotNames(ind)]
 
   # find the highest age across measurements
   rng <- c(NA, NA)
   for (yname in ynames) {
-    v <- slot(slot(x, yname), slotname)
+    v <- slot(slot(ind, yname), slotname)
     if (sum(!is.na(v)) > 0L) rng <- range(c(v, rng), na.rm = TRUE, finite = TRUE)
   }
 
