fc4840cc87e7c8652b97d5b24e212bba27687937
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-07-17 08:15:58 +0200

    Add convert_bds_individual()

--- R/imports.R
+++ R/imports.R
@@ -1,8 +1,11 @@
 #' @import methods
-#' @importFrom donorloader load_child_data load_time_data
-#' @importFrom stats predict
-#' @importFrom dplyr full_join arrange distinct %>%
-#' @importFrom clopus y2z z2y create.reference.call is.reference
-#' @importFrom brokenstick get_knots
-#' @importFrom rlang .data
+#' @importFrom brokenstick     get_knots
+#' @importFrom clopus          y2z z2y create.reference.call
+#'                             is.reference
+#' @importFrom donorloader     load_child_data load_time_data
+#' @importFrom dplyr           full_join arrange distinct %>%
+#' @importFrom jsonlite        fromJSON
+#' @importFrom lubridate       ymd dmy
+#' @importFrom rlang           .data
+#' @importFrom stats           predict
 NULL
