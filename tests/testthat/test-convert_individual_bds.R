6ec9c44a9f9defd3b455428a3dc453e8051405eb
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-07-20 14:48:33 +0200

    Add tests

--- tests/testthat/test-convert_individual_bds.R
+++ tests/testthat/test-convert_individual_bds.R
@@ -1,11 +1,11 @@
-library(minihealth)
-fn <- paste0("file://", file.path(path.package("minihealth"), "testdata", "client3.json"))
-con <- curl::curl(fn, open = "r")
-txt <- readLines(con)
-d <- jsonlite::fromJSON(txt)
-b <- d$ClientGegevens$Elementen
-
-data("installed.cabinets", package = "jamestest")
-ind <- installed.cabinets[[2]][[1]]
-
-z <- convert_individual_bds(ind)
+# library(minihealth)
+# fn <- paste0("file://", file.path(path.package("minihealth"), "testdata", "client3.json"))
+# con <- curl::curl(fn, open = "r")
+# txt <- readLines(con)
+# d <- jsonlite::fromJSON(txt)
+# b <- d$ClientGegevens$Elementen
+#
+# data("installed.cabinets", package = "jamestest")
+# ind <- installed.cabinets[[2]][[1]]
+#
+# z <- convert_individual_bds(ind)
---
+++ tests/testthat/test-donordata.to.individual.R
@@ -0,0 +1,32 @@
+context("donordata.to.individual")
+
+library(donordata)
+library(donorloader)
+library(jamestest)
+
+con <- NULL
+dnr <- "smocc"
+ids <- as.integer(c(34071, 34072, 34073, 34075, 34076, 34077, 34078, 34079, 34080, 34081))
+names <- c("Laura S", "Thomas S", "Anne S", "Jeroen S",
+           "Mark S", "Kevin S", "Linda S", "Iris S",
+           "Tim S", "Rick S")
+
+child <- load_child_data(con = con, dnr = dnr, ids = ids[1])
+time <- load_time_data(con = con, dnr = dnr, ids = ids[1])
+
+laura <- donordata.to.individual(dnr = dnr, id = ids[1])
+kevin <- donordata.to.individual(dnr = dnr, id = ids[6])
+
+# lollypop.preterm
+dnr <- "lollypop.preterm"
+ids <- as.integer(c(53696, 53675, 53676, 53684, 53519, 53520, 53531, 53582, 53583, 53584))
+names <- c("Jurre P", "Sanne P",  "Milan P", "Roos P",  "Bram P",
+           "Freek P", "Anouk P",  "Sharon P", "Nick P", "Simon P")
+
+id <- ids[1]
+child <- load_child_data(con = con, dnr = dnr, ids = id)
+time <- load_time_data(con = con, dnr = dnr, ids = id)
+
+jurre <- donordata.to.individual(dnr = dnr, id = ids[1])
+anouk <- donordata.to.individual(dnr = dnr, id = ids[6])
+
