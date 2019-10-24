67c2ee18696e30f963514fe6a89af2d4c6268692
Arjan Huizing <mail@arjanhuizing.com>
2019-10-24 11:20:10 +0200

    improved formatting

--- R/validate_bds_individual.R
+++ R/validate_bds_individual.R
@@ -9,14 +9,21 @@
 #' @author Arjan Huizing 2019
 #' @export
 validate_bds_individual <- function(txt = NULL, verbose = TRUE){
+
   bds_schema <- file.path(path.package("minihealth"), "data", "bds_schema")
-  valid <- jsonvalidate::json_validate(txt, schema = bds_schema, engine = "ajv", verbose = T)
+  valid <- jsonvalidate::json_validate(txt, schema = bds_schema,
+                                       engine = "ajv", verbose = verbose)
+
   if(valid | !verbose){
     return(valid)
   } else{
-    # extract error and convert from list to data.frame
-    warnings <- valid %>% attr("error") %>% filter(keyword %in% c("anyOf", "required")) %>%
+
+    # extract error information
+    warnings <- valid %>% attr("error") %>%
+      filter(keyword %in% c("anyOf", "required")) %>%
       select(data) %>% simplify2array
+
+    # convert from list to data.frame
     user.warning <- data.frame()
     for(i in 1:length(warnings)){
       user.warning[i, "Bdsnummer"] <- warnings[i,1]$data$Bdsnummer
@@ -25,6 +32,7 @@ validate_bds_individual <- function(txt = NULL, verbose = TRUE){
       user.warning[i, "Supplied.class"] <- ifelse(is.null(warnings[i,1]$data$Waarde),
                                           NA, class(warnings[i,1]$data$Waarde))
     }
+
     attr(valid, "errors") <- user.warning
     return(valid)
   }
