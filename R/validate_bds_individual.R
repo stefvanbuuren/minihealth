b9e0c99bf3e6144e3a9c26ce9622a30699bb9b99
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-11-14 09:22:53 +0100

    Evade keyword and data globals

--- R/validate_bds_individual.R
+++ R/validate_bds_individual.R
@@ -9,30 +9,35 @@
 #' @return A \code{boolean} with optional \code{errors} attribute.
 #' @author Arjan Huizing 2019
 #' @export
-validate_bds_individual <- function(txt = NULL, verbose = TRUE, schema = "default"){
+validate_bds_individual <- function(txt = NULL, verbose = TRUE,
+                                    schema = "default"){
 
+  path <- file.path(path.package("minihealth"), "json")
   bds_schema <- switch(schema,
-                       "default" = file.path(path.package("minihealth"), "json", "bds_schema.json"),
-                       "string" = file.path(path.package("minihealth"), "json", "bds_schema_str.json"))
-  valid <- jsonvalidate::json_validate(txt, bds_schema, engine = "ajv", verbose = verbose)
+                       "default" = file.path(path, "bds_schema.json"),
+                       "string"  = file.path(path, "bds_schema_str.json"))
+  valid <- jsonvalidate::json_validate(txt, bds_schema, engine = "ajv",
+                                       verbose = verbose)
 
   if(valid | !verbose){
     return(valid)
   } else{
 
     # extract error information
-    warnings <- valid %>% attr("error") %>%
-      filter(keyword %in% c("anyOf", "required")) %>%
-      select(data) %>% simplify2array
+    warnings <- valid %>%
+      attr("error") %>%
+      filter(.data$keyword %in% c("anyOf", "required")) %>%
+      select(.data$data) %>%
+      simplify2array()
 
     # convert from list to data.frame
     user.warning <- data.frame()
-    for(i in 1:length(warnings)){
-      user.warning[i, "Bdsnummer"] <- warnings[i,1]$data$Bdsnummer
-      user.warning[i, "Waarde"] <- ifelse(is.null(warnings[i,1]$data$Waarde),
-                                          NA, as.character(warnings[i,1]$data$Waarde))
-      user.warning[i, "Mode"] <- ifelse(is.null(warnings[i,1]$data$Waarde),
-                                          NA, mode(warnings[i,1]$data$Waarde))
+    for(i in seq_along(warnings)) {
+      user.warning[i, "Bdsnummer"] <- warnings[i, 1]$data$Bdsnummer
+      user.warning[i, "Waarde"] <- ifelse(is.null(warnings[i, 1]$data$Waarde),
+                                          NA, as.character(warnings[i, 1]$data$Waarde))
+      user.warning[i, "Mode"] <- ifelse(is.null(warnings[i, 1]$data$Waarde),
+                                          NA, mode(warnings[i, 1]$data$Waarde))
     }
 
     attr(valid, "errors") <- user.warning
