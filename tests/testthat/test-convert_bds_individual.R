bf60794d36982d3caeb8405fc69476246c08401f
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-11-20 17:53:01 +0100

    Add tests for test4-test10.json

--- tests/testthat/test-convert_bds_individual.R
+++ tests/testthat/test-convert_bds_individual.R
@@ -30,23 +30,38 @@ test_that("test3.json (missing OrganisatieCode) FAILS",
           expect_error(convert_bds_individual(jtf[3], schema = "string"),
                        "should have required property 'OrganisatieCode'"))
 
-# test_that("test4.json (wrong type) FAILS",
-#           expect_false(convert_bds_individual(jtf[4]), "individual"))
-#
-# test_that("test5.json (missing ClientGegevens) FAILS",
-#           expect_false(convert_bds_individual(jtf[5]), "individual"))
-#
-# test_that("test6.json (Missing ContactMomenten) PASSES",
-#           expect_true(convert_bds_individual(jtf[6]), "individual"))
+test_that("test4.json (wrong type) FAILS",
+          expect_error(convert_bds_individual(jtf[4], schema = "string"),
+                       ".OrganisatieCode should be integer"))
 
+test_that("test5.json (missing ClientGegevens) FAILS",
+          expect_error(convert_bds_individual(jtf[5], schema = "string"),
+                       "should have required property 'ClientGegevens'"))
+
+test_that("test6.json (Missing ContactMomenten) PASSES",
+          expect_s4_class(convert_bds_individual(jtf[6], schema = "string"), "individual"))
+
+test_that("test7.json (Missing Referentie & OrganisatieCode) FAILS",
+          expect_error(convert_bds_individual(jtf[7], schema = "string"),
+                       "should have required property 'OrganisatieCode'"))
 
 test_that("test8.json returns error message",
-          expect_error(convert_bds_individual(jtf[8]), "lexical error: invalid char in json text.")
-)
+          expect_error(convert_bds_individual(jtf[8], schema = "string"), "lexical error: invalid char in json text."))
+
+test_that("test9.json (Bdsnummer 19 missing) FAILS",
+          expect_error(convert_bds_individual(jtf[9], schema = "string"),
+                       "Required BDS number(s) missing: 19", fixed = TRUE))
+
+test_that("test10.json (Bdsnummer 20 missing) FAILS",
+          expect_error(convert_bds_individual(jtf[10], schema = "string"),
+                       "Required BDS number(s) missing: 20", fixed = TRUE))
+
+# test_that("test11.json (Bdsnummer 82 missing) PASSES",
+#           expect_s4_class(convert_bds_individual(jtf[11], schema = "string"), "individual"))
+
 
 test_that("test14.json return error message",
-          expect_error(convert_bds_individual(jtf[14]), "premature EOF")
-)
+          expect_error(convert_bds_individual(jtf[14]), "premature EOF"))
 
 # test_that("minimal file test21.json turns into S4-object",
 #           expect_s4_class(convert_bds_individual(jtf[21]), "individual")
