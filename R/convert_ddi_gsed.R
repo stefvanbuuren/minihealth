6183595c8a06e55599c5b070da0c1259e3194eb9
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-12-12 22:17:56 +0100

    Return zero rows instead of NULL

--- R/convert_ddi_gsed.R
+++ R/convert_ddi_gsed.R
@@ -1,15 +1,20 @@
 
 convert_ddi_gsed <- function(d, r) {
 
-  # if (length(d$Contactmomenten) == 0L) return(NULL)
-
-  # prepare the output matrices
+  # get BDS numbers and names of items
   bds <- sort(unique(minihealth::bds_gsed$bds))
   items <- minihealth::bds_gsed$lex_gsed[minihealth::bds_gsed$lex_gsed != ""]
-  agedays <- r$dom - r$dob
-  w <- matrix(NA, nrow = length(agedays), ncol = 1L + length(bds) + length(items),
-              dimnames = list(NULL, c("agedays", bds, items)))
-  w[, "agedays"] <- agedays
+
+  # premature return if there are no data
+  if (length(d$Contactmomenten) == 0L)
+    return(data.frame(age = numeric(0)))
+
+  # prepare the output matrices
+  age <- as.numeric(round((r$dom - r$dob) / 365.25, 4L))
+  w <- data.frame(
+    age = age,
+    matrix(NA, nrow = length(age), ncol = length(bds) + length(items)))
+  names(w) <- c("age", bds, items)
 
   # extract ddi data from bds-message
   # and convert to 0/1 scores
@@ -19,22 +24,22 @@ convert_ddi_gsed <- function(d, r) {
     type <- minihealth::bds_gsed[n, "type"]
     bds  <- minihealth::bds_gsed[n, c("bds", "bdsr", "bdsl")]
     w[, item] <- switch(type,
-      g1 = recode(w[, as.character(bds[1L])], `1` = 1L, `2` = 0L),
-      m1 = recode(w[, as.character(bds[1L])], `1` = 1L, `2` = 0L, `3` = 1L),
-      g2 = {
-        bdsr <- w[, as.character(bds[2L])]
-        bdsl <- w[, as.character(bds[3L])]
-        pass <- as.numeric(bdsr == 1L & bdsl == 1L)
-        pass[is.na(bdsr) & is.na(bdsl)] <- NA
-        pass},
-      m2 = {
-        bdsr <- w[, as.character(bds[2L])]
-        bdsl <- w[, as.character(bds[3L])]
-        pass <- as.numeric(bdsr %in% c(1L, 3L) & bdsl %in% c(1L, 3L))
-        pass[is.na(bdsr) & is.na(bdsl)] <- NA
-        pass},
-      stop("Unrecognized type ", type, " for item ", item)
+                        g1 = recode(w[, as.character(bds[1L])], `1` = 1L, `2` = 0L),
+                        m1 = recode(w[, as.character(bds[1L])], `1` = 1L, `2` = 0L, `3` = 1L),
+                        g2 = {
+                          bdsr <- w[, as.character(bds[2L])]
+                          bdsl <- w[, as.character(bds[3L])]
+                          pass <- as.numeric(bdsr == 1L & bdsl == 1L)
+                          pass[is.na(bdsr) & is.na(bdsl)] <- NA
+                          pass},
+                        m2 = {
+                          bdsr <- w[, as.character(bds[2L])]
+                          bdsl <- w[, as.character(bds[3L])]
+                          pass <- as.numeric(bdsr %in% c(1L, 3L) & bdsl %in% c(1L, 3L))
+                          pass[is.na(bdsr) & is.na(bdsl)] <- NA
+                          pass},
+                        stop("Unrecognized type ", type, " for item ", item)
     )
   }
-  w[, c("agedays", items)]
+  w[, c("age", items)]
 }
