e4a5e45ba37fb56efe27bc698eb445fed30fb551
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-12-16 17:22:43 +0100

    Extend bse class with clopus::transform_y() and clopus::transform_z()

--- R/class-bse.R
+++ R/class-bse.R
@@ -13,18 +13,24 @@ NULL
 #'
 #'@section Slots:
 #'  \describe{
-#'    \item{\code{call}:}{An object of class \code{call} that refers to an object with a pre-fitted broken stick model. Evaluation by \code{eval(call)} should yield an object of class \code{brokenstick} as created by
+#'    \item{\code{call}:}{An object of class \code{call} that refers to an object
+#'    with a pre-fitted broken stick model. Evaluation by \code{eval(call)} should
+#'    yield an object of class \code{brokenstick} as created by
 #'    \code{\link[brokenstick]{brokenstick}}.}
-#'    \item{\code{found}:}{Logical indicating whether the broken stick model specified by
-#'    \code{call} was actually found.}
-#'    \item{\code{zscale}:}{A logical indicating whether the broken stick model models the outcome in the Z-scale (\code{zscale = TRUE}, default) or Y-scale (\code{zscale = FALSE}). At initialization, the \code{new()} function tries to infer and set this flag from the model specified by \code{call}.}
+#'    \item{\code{found}:}{Logical indicating whether the broken stick model
+#'    specified by \code{call} was actually found.}
+#'    \item{\code{zscale}:}{A logical indicating whether the broken stick model
+#'    models the outcome in the Z-scale (\code{zscale = TRUE}, default) or Y-scale
+#'    (\code{zscale = FALSE}). At initialization, the \code{new()} function tries
+#'    to infer and set this flag from the model specified by \code{call}.}
 #'}
 #'
 #'@name bse-class
 #'@rdname bse-class
 #'@aliases bse-class
 #'@author Stef van Buuren 2016
-#'@seealso \code{\link{xyz-class}}, \code{\link[brokenstick]{brokenstick}}, \code{\link[brokenstick]{predict.brokenstick}}
+#'@seealso \code{\link{xyz-class}}, \code{\link[brokenstick]{brokenstick}},
+#'\code{\link[brokenstick]{predict.brokenstick}}
 #'@keywords classes
 #'@examples
 #'library(donorloader)
@@ -36,7 +42,7 @@ NULL
 #' child
 #'
 #' boy <- new("xyz", x = c(0, 0.2, 0.5), y = c(51.0, 54.1, 63.4), sex = "male")
-#' girl <- new("xyz", x = c(0, 0.2, 0.5), y = c(51.0, 54.1, 63.4), sex = "female", ga = 32)
+#' girl <- new("xyz", x = c(0, 0.2, 0.5), y = c(51.0, 54.1, 63.4), sex = "female")
 #' boy
 #' girl
 #'
@@ -55,8 +61,14 @@ NULL
 #'d4 <- new("bse", new("xyz", x = c(0, 0.2, 0.5), y = c(51.0, 54.1, 63.4),
 #' sex = "female"), at = "knots")
 #'d4
+#'
+#'# use transform_y function instead of reference call
+#'# now, -0.126 is converted to 41.2 cm
+#' d5 <- new("bse", girl, usetransform = TRUE, ga = 32, sex = "female")
+#' d5
+#' d5@transform
+#'
 #'@export
-
 setClass("bse",
          contains = "xyz",
          slots = c(
@@ -75,8 +87,13 @@ setMethod("initialize", "bse",
                     at = c("x", "knots"),
                     models = "smocc_bs",
                     call = quote(as.numeric(NULL)),
+                    usetransform = FALSE,
                     ...) {
             at <- match.arg(at)
+
+            # store flag
+            slot(.Object, "usetransform") <- usetransform
+            slot(.Object, "transform") <- "not set"
 
             # can't calculate if we have no data
             if (missing(data)) data <- new("xyz", ...)
@@ -88,6 +105,13 @@ setMethod("initialize", "bse",
             .Object@xname <- data@xname
             .Object@yname <- data@yname
             .Object@zname <- data@zname
+
+            # parse sex and ga
+            catch <- function(sex = NA_character_, ga = NA_real_, ...) {
+              sex <- match.arg(sex, c("male", "female", NA_character_))
+              list(sex = sex, ga = as.numeric(ga))
+            }
+            sexga <- catch(...)
 
             # direct call specification overides everything else
             if (!missing(call)) slot(.Object, "call") <- as.call(call)
@@ -106,7 +130,8 @@ setMethod("initialize", "bse",
             # fill only if we have a model
             if (.Object@found) {
 
-              # Was the model in the Z-score scale
+              # Was the model in the Z-score scale?
+              # Not functional: we always model in the Z-score metric
               if (is.null(model$zmodel)) .Object@zscale <- TRUE
               else .Object@zscale <- model$zmodel
 
@@ -115,7 +140,7 @@ setMethod("initialize", "bse",
               if (at == "knots") .Object@x <- as.numeric(get_knots(model))
 
               if (.Object@zscale) {
-                # z scale
+                # transform z into y
                 df <- data.frame(
                   age = data@x,
                   zname = data@z,
@@ -126,13 +151,26 @@ setMethod("initialize", "bse",
                   if (at == "knots") .Object@z <- predict(model, df, x = "knots", shape = "vector", ...)
                 }
                 else .Object@z <- numeric(0)
-                if (length(.Object@z) == 0) .Object@x <- numeric(0)
-                .Object@y <- as.numeric(z2y(z = .Object@z,
-                                            x = .Object@x,
-                                            ref = eval(data@call),
-                                            ...))
+                if (length(.Object@z) == 0) {
+                  .Object@x <- numeric(0)
+                  .Object@y <- numeric(0)
+                } else if (usetransform) {
+                  slot(.Object, "transform") <- "transform_y()"
+                  df <- data.frame(z = .Object@z,
+                                   x = .Object@x,
+                                   sex = sexga$sex,
+                                   ga = sexga$ga)
+                  names(df) <- c(paste0(.Object@yname, ".z"), "age", "sex", "ga")
+                  slot(.Object, "y") <-
+                    as.numeric(transform_y(df, ynames = .Object@yname)[[.Object@yname]])
+                } else {
+                  .Object@y <- as.numeric(z2y(z = .Object@z,
+                                              x = .Object@x,
+                                              ref = eval(data@call),
+                                              ...))
+                }
               } else {
-                # y scale
+                # transform y into z
                 df <- data.frame(
                   age = data@x,
                   yname = data@y,
@@ -143,11 +181,24 @@ setMethod("initialize", "bse",
                   if (at == "knots") .Object@y <- predict(model, df, x = "knots", shape = "vector", ...)
                 }
                 else .Object@y <- numeric(0)
-                if (length(.Object@y) == 0) .Object@y <- numeric(0)
-                .Object@z <- as.numeric(y2z(y = .Object@y,
-                                            x = .Object@x,
-                                            ref = eval(data@call),
-                                            ...))
+                if (length(.Object@y) == 0) {
+                  .Object@x <- numeric(0)
+                  .Object@z <- numeric(0)
+                } else if (usetransform) {
+                  slot(.Object, "transform") <- "transform_z()"
+                  df <- data.frame(y = .Object@y,
+                                   x = .Object@x,
+                                   sex = sexga$sex,
+                                   ga = sexga$ga)
+                  names(df) <- c(.Object@yname, "age", "sex", "ga")
+                  slot(.Object, "z") <-
+                    as.numeric(transform_z(df, ynames = .Object@yname)[[paste0(.Object@yname, ".z")]])
+                } else {
+                  .Object@z <- as.numeric(y2z(y = .Object@y,
+                                              x = .Object@x,
+                                              ref = eval(data@call),
+                                              ...))
+                }
               }
             }
 
